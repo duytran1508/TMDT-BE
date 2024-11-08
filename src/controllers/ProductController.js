f7ed16620fd40ed4a5d48c1c63e86256e9a52792
Hữu Hào <phanhao@192.168.0.4.non-exists.ptr.local>
2024-11-08 19:27:57 +0700

    fix bug json

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,122 +1,154 @@
 const ProductService = require("../services/ProductService");
+const admin = require("firebase-admin");
+const multer = require("multer");
+require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
+const serviceAccount = require("../config/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+});
+
+const bucket = admin.storage().bucket();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const uploadProductImages = upload.fields([
+  { name: "image", maxCount: 1 },
+  { name: "banner", maxCount: 1 }
+]);
 
 const createProduct = async (req, res) => {
   try {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    const response = await ProductService.createProduct(req.body);
-    return res.status(201).json(response);
-=======
-    const {
-      name,
-      productsTypeName,
-      quantityInStock,
-      prices,
-      size,
-      color,
-      imageUrl,
-      bannerUrl,
-      brand,
-      gender
-    } = req.body;
-    if (
-      !name ||
-      !productsTypeName ||
-      !quantityInStock ||
-      !prices ||
-      !size ||
-      !color ||
-      !brand ||
-      !gender
-    ) {
-      return res.status(400).json({
-        status: "ERR",
-        message: "The input is required"
-      });
+    const data = { ...req.body };
+
+    // Kiểm tra nếu có ảnh mới được tải lên và xử lý
+    if (req.files) {
+      if (req.files["image"] && req.files["image"].length > 0) {
+        const imageFile = req.files["image"][0];
+        const imageFileName = `${Date.now()}-${imageFile.originalname}`;
+        const fileUpload = bucket.file(imageFileName);
+        const token = uuidv4();
+
+        await fileUpload.save(imageFile.buffer, {
+          contentType: imageFile.mimetype,
+          metadata: {
+            firebaseStorageDownloadTokens: token
+          }
+        });
+
+        data.imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+        }/o/${encodeURIComponent(imageFileName)}?alt=media&token=${token}`;
+      }
+
+      if (req.files["banner"] && req.files["banner"].length > 0) {
+        const bannerFile = req.files["banner"][0];
+        const bannerFileName = `${Date.now()}-${bannerFile.originalname}`;
+        const fileUpload = bucket.file(bannerFileName);
+        const bannerToken = uuidv4();
+
+        await fileUpload.save(bannerFile.buffer, {
+          contentType: bannerFile.mimetype,
+          metadata: {
+            firebaseStorageDownloadTokens: bannerToken
+          }
+        });
+
+        data.bannerUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+        }/o/${encodeURIComponent(
+          bannerFileName
+        )}?alt=media&token=${bannerToken}`;
+      }
     }
 
-    const fs = require("fs");
-    const path = require("path");
-
-    const saveBase64Image = (base64Data, filename, isImageFile) => {
-      const base64String = base64Data.split(";base64,").pop();
-
-      const folder = isImageFile ? "images" : "slides";
-      const filePath = path.join(__dirname, "../uploads", folder, filename);
-
-      const uploadsDir = path.join(__dirname, "../uploads");
-      const specificDir = path.join(uploadsDir, folder);
-
-      if (!fs.existsSync(uploadsDir)) {
-        fs.mkdirSync(uploadsDir);
-      }
-
-      if (!fs.existsSync(specificDir)) {
-        fs.mkdirSync(specificDir);
-      }
-
-      fs.writeFileSync(filePath, base64String, { encoding: "base64" });
-
-      return filePath;
-    };
-
-    const imageFilePath = saveBase64Image(
-      imageUrl,
-      `${Date.now()}_image.jpg`,
-      true
-    );
-    const bannerFilePath = saveBase64Image(
-      bannerUrl,
-      `${Date.now()}_banner.jpg`,
-      false
-    );
-
-    const productData = {
-      name,
-      quantityInStock,
-      prices,
-      size,
-      color,
-      imageUrl: imageFilePath,
-      bannerUrl: bannerFilePath,
-      brand,
-      gender
-    };
-
-    const response = await ProductService.createProduct(productData);
-    return res.status(201).json({
-      status: "OK",
-      message: "Product created successfully",
-      data: response
-    });
->>>>>>> 23d369f40e25c5a316949cca646da96a481eae9b
-=======
-    const response = await ProductService.createProduct(req.body);
-    return res.status(201).json(response);
->>>>>>> 3d39295 (done CRUD category and CRUD product)
-  } catch (e) {
-    return res.status(500).json({
-      status: "ERR",
-      message: e.message || "An error occurred while creating the product"
-    });
+    const result = await ProductService.createProduct(data);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Đã xảy ra lỗi khi thêm sản phẩm: " + error.message);
   }
 };
 
 const updateProduct = async (req, res) => {
+  const dataUpdate = { ...req.body };
+
+  if (
+    Object.keys(dataUpdate).length === 0 &&
+    (!req.files || Object.keys(req.files).length === 0)
+  ) {
+    return res.status(400).send({ message: "Không có dữ liệu" });
+  }
+
   try {
-<<<<<<< HEAD
-    const response = await ProductService.updateProduct(req.params.id, req.body);
-=======
-    const response = await ProductService.updateProduct(
-      req.params.id,
-      req.body
-    );
->>>>>>> 3d39295 (done CRUD category and CRUD product)
-    return res.status(200).json(response);
-  } catch (e) {
+    const productId = req.params.id;
+    if (!productId) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "Product ID is required."
+      });
+    }
+
+    // Cập nhật imageUrl nếu có file mới
+    if (req.files && req.files["image"] && req.files["image"].length > 0) {
+      const imageFile = req.files["image"][0];
+      const imageFileName = `${Date.now()}-${imageFile.originalname}`;
+      const fileUpload = bucket.file(imageFileName);
+      const token = uuidv4();
+
+      await fileUpload.save(imageFile.buffer, {
+        contentType: imageFile.mimetype,
+        metadata: {
+          firebaseStorageDownloadTokens: token
+        }
+      });
+
+      dataUpdate.imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+        process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+      }/o/${encodeURIComponent(imageFileName)}?alt=media&token=${token}`;
+    }
+
+    // Cập nhật bannerUrl nếu có file mới
+    if (req.files && req.files["banner"] && req.files["banner"].length > 0) {
+      const bannerFile = req.files["banner"][0];
+      const bannerFileName = `${Date.now()}-${bannerFile.originalname}`;
+      const fileUpload = bucket.file(bannerFileName);
+      const bannerToken = uuidv4();
+
+      await fileUpload.save(bannerFile.buffer, {
+        contentType: bannerFile.mimetype,
+        metadata: {
+          firebaseStorageDownloadTokens: bannerToken
+        }
+      });
+
+      dataUpdate.bannerUrl = `https://firebasestorage.googleapis.com/v0/b/${
+        process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+      }/o/${encodeURIComponent(bannerFileName)}?alt=media&token=${bannerToken}`;
+    }
+
+    // Xử lý nếu không có file mới, loại bỏ URL không cập nhật
+    if (typeof dataUpdate.imageUrl !== "string") {
+      delete dataUpdate.imageUrl;
+    }
+    if (typeof dataUpdate.bannerUrl !== "string") {
+      delete dataUpdate.bannerUrl;
+    }
+
+    const result = await ProductService.updateProduct(productId, dataUpdate);
+    if (result.status === "ERR") {
+      return res.status(404).json(result);
+    }
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error("Error updating product:", error);
     return res.status(500).json({
       status: "ERR",
-      message: e.message || "An error occurred while updating the product"
+      message: "Error updating product",
+      error: error.message
     });
   }
 };
@@ -125,24 +157,40 @@ const deleteProduct = async (req, res) => {
   try {
     const productId = req.params.id;
     if (!productId) {
-      return res.status(200).json({
+      return res.status(400).json({
         status: "ERR",
-        message: "the userId is required "
+        message: "The productId is required."
       });
     }
     const response = await ProductService.deleteProduct(productId);
     return res.status(200).json(response);
   } catch (e) {
-    return res.status(404).json({
-      message: e
+    return res.status(500).json({
+      status: "ERR",
+      message: e.message
     });
   }
 };
 
-<<<<<<< HEAD
+const deleteManyProduct = async (req, res) => {
+  try {
+    const ids = req.body;
+    if (!ids || !Array.isArray(ids) || ids.length === 0) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "The ids are required and should be an array."
+      });
+    }
+    const response = await ProductService.deleteManyProduct(ids);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(500).json({
+      status: "ERR",
+      message: e.message
+    });
+  }
+};
 
-=======
->>>>>>> 3d39295 (done CRUD category and CRUD product)
 const getAllProduct = async (req, res) => {
   try {
     const { sort, filter } = req.query;
@@ -151,6 +199,7 @@ const getAllProduct = async (req, res) => {
   } catch (error) {
     console.error("Error fetching products:", error);
     return res.status(500).json({
+      status: "ERR",
       message: "An error occurred while fetching products.",
       error: error.message
     });
@@ -161,29 +210,40 @@ const getDetailsProduct = async (req, res) => {
   try {
     const productId = req.params.id;
     if (!productId) {
-      return res.status(200).json({
+      return res.status(400).json({
         status: "ERR",
-        message: "the productId is required "
+        message: "The productId is required."
       });
     }
     const response = await ProductService.getDetailsProduct(productId);
     return res.status(200).json(response);
   } catch (e) {
-    return res.status(404).json({
-      message: e
+    return res.status(500).json({
+      status: "ERR",
+      message: e.message
     });
   }
 };
 
-<<<<<<< HEAD
+const getAllType = async (req, res) => {
+  try {
+    const response = await ProductService.getAllType();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(500).json({
+      status: "ERR",
+      message: e.message
+    });
+  }
+};
 
-
-=======
->>>>>>> 3d39295 (done CRUD category and CRUD product)
 module.exports = {
+  uploadProductImages,
   createProduct,
   updateProduct,
   getDetailsProduct,
   deleteProduct,
-  getAllProduct
+  deleteManyProduct,
+  getAllProduct,
+  getAllType
 };
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,129 +1,206 @@
 const Product = require("../models/ProductModel");
+const fs = require("fs");
 
-const createProduct = (productData) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const newProduct = await Product.create(productData);
-      resolve({
-        status: "OK",
-        message: "Product created successfully",
-        data: newProduct
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
+const createProduct = async (newProduct) => {
+  const {
+    name,
+    quantityInStock,
+    prices,
+    imageUrl,
+    bannerUrl,
+    productsTypeName,
+    inches,
+    screenResolution,
+    company,
+    cpu,
+    ram,
+    memory,
+    gpu,
+    weight,
+    opsys
+  } = newProduct;
+
+  try {
+    const createdProduct = await Product.create({
+      name: name || "",
+      quantityInStock: quantityInStock || 0,
+      prices: prices || 0,
+      imageUrl: imageUrl || "",
+      bannerUrl: bannerUrl || "",
+      productsTypeName: productsTypeName || "",
+      inches: inches || "",
+      screenResolution: screenResolution || "",
+      company: company || "",
+      cpu: cpu || "",
+      ram: ram || "",
+      memory: memory || "",
+      gpu: gpu || "",
+      weight: weight || "",
+      opsys: opsys || ""
+    });
+
+    return {
+      status: "OK",
+      message: "Product created successfully",
+      data: createdProduct
+    };
+  } catch (error) {
+    return {
+      status: "ERR",
+      message: "Failed to create product",
+      error: error.message
+    };
+  }
 };
 
-const updateProduct = (id, data) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const checkProduct = await Product.findOne({
-        _id: id
-      });
-      if (checkProduct === null) {
-        resolve({
-          status: "Oke",
-          message: "Product is not defined"
-        });
-      }
-
-      const updatedProduct = await Product.findByIdAndUpdate(id, data, {
-        new: true
-      });
-
-      resolve({
-        status: "Oke",
-        massage: "Success",
-        data: updatedProduct
-      });
-    } catch (e) {
-      reject(e);
+const updateProduct = async (id, data) => {
+  try {
+    const checkProduct = await Product.findById(id);
+    if (!checkProduct) {
+      return {
+        status: "ERR",
+        message: "Product not found"
+      };
     }
-  });
-};
-const deleteProduct = (id) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const checkProduct = await Product.findOne({
-        _id: id
-      });
-      if (checkProduct === null) {
-        resolve({
-          status: "Oke",
-          message: "Product is not defined"
-        });
-      }
 
-      await Product.findByIdAndDelete(id);
-      resolve({
-        status: "Oke",
-        massage: "delete success"
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
+    const updatedProduct = await Product.findByIdAndUpdate(id, data, {
+      new: true
+    });
+
+    return {
+      status: "OK",
+      message: "Product updated successfully",
+      data: updatedProduct
+    };
+  } catch (e) {
+    return {
+      status: "ERR",
+      message: "Error updating product",
+      error: e.message
+    };
+  }
 };
 
-<<<<<<< HEAD
-
-const getAllProduct = () => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const products = await Product.find().populate('category');
-=======
-const getAllProduct = () => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const products = await Product.find().populate("category");
->>>>>>> 3d39295 (done CRUD category and CRUD product)
-      resolve({
-        status: "OK",
-        message: "Retrieved all products successfully",
-        data: products
-      });
-    } catch (e) {
-      reject(e);
+const deleteProduct = async (id) => {
+  try {
+    const checkProduct = await Product.findById(id);
+    if (!checkProduct) {
+      return {
+        status: "ERR",
+        message: "Product not found"
+      };
     }
-  });
-};
-const getDetailsProduct = (id) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const product = await Product.findOne({
-        _id: id
-      });
-      if (product === null) {
-        resolve({
-          status: "Oke",
-          message: "Product is not defined"
-        });
-      }
 
-      resolve({
-        status: "Oke",
-        massage: "success",
-        data: product
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
+    await Product.findByIdAndDelete(id);
+    return {
+      status: "OK",
+      message: "Product deleted successfully"
+    };
+  } catch (e) {
+    return {
+      status: "ERR",
+      message: "Error deleting product",
+      error: e.message
+    };
+  }
 };
 
-<<<<<<< HEAD
+const deleteManyProduct = async (ids) => {
+  try {
+    await Product.deleteMany({ _id: { $in: ids } });
+    return {
+      status: "OK",
+      message: "Products deleted successfully"
+    };
+  } catch (e) {
+    return {
+      status: "ERR",
+      message: "Error deleting products",
+      error: e.message
+    };
+  }
+};
 
-=======
->>>>>>> 3d39295 (done CRUD category and CRUD product)
+const getAllProduct = async (sort, filter) => {
+  try {
+    let query = {};
+    if (filter) {
+      const [label, value] = filter;
+      query[label] = { $regex: value, $options: "i" };
+    }
+
+    const allProducts = await Product.find(query)
+      .sort(sort || {})
+      .populate("category");
+
+    const formattedProducts = allProducts.map((product) => ({
+      ...product.toObject(),
+      imageUrl: product.imageUrl || null,
+      bannerUrl: product.bannerUrl || null
+    }));
+
+    return {
+      status: "OK",
+      message: "Products retrieved successfully",
+      data: formattedProducts,
+      total: formattedProducts.length
+    };
+  } catch (e) {
+    return {
+      status: "ERR",
+      message: "Error retrieving products",
+      error: e.message
+    };
+  }
+};
+
+const getDetailsProduct = async (id) => {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return {
+        status: "ERR",
+        message: "Product not found"
+      };
+    }
+
+    return {
+      status: "OK",
+      message: "Product details retrieved successfully",
+      data: product
+    };
+  } catch (e) {
+    return {
+      status: "ERR",
+      message: "Error retrieving product details",
+      error: e.message
+    };
+  }
+};
+
+const getAllType = async () => {
+  try {
+    const allTypes = await Product.distinct("type");
+    return {
+      status: "OK",
+      message: "Product types retrieved successfully",
+      data: allTypes
+    };
+  } catch (e) {
+    return {
+      status: "ERR",
+      message: "Error retrieving product types",
+      error: e.message
+    };
+  }
+};
+
 module.exports = {
   createProduct,
   updateProduct,
-  getDetailsProduct,
   deleteProduct,
-<<<<<<< HEAD
+  deleteManyProduct,
   getAllProduct,
-=======
-  getAllProduct
->>>>>>> 3d39295 (done CRUD category and CRUD product)
+  getDetailsProduct,
+  getAllType
 };
